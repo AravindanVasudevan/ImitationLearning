b579098eda8bf137b8c2d035e7e79c649eb8c5d9
Aravindan Vasudevan <aravindan.vasudevan98@gmail.com>
2023-09-12 20:29:52 -0500

    Adding files

---
+++ model.py
@@ -0,0 +1,22 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class IMLNN(nn.Module):
+    def __init__(self, input_dims, n_actions, fc1_dims = 128, fc2_dims = 128):
+        super(IMLNN, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+
+        self.fc1 = nn.Linear(self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.fc3 = nn.Linear(self.fc2_dims, self.n_actions)
+
+    def forward(self, state):
+        x = F.relu(self.fc1(state))
+        x = F.relu(self.fc2(x))
+        actions = self.fc3(x)
+
+        return actions
